453f5a6eb84c5b310b847f622c8163ad0ad37e67
odan <odan3240@gmail.com>
2018-03-16 10:27:14 +0900

    あいまい検索に対応

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import os
 import sys
-from time import time
 
 
 if 'VENDOR_PATH' in os.environ:
@@ -14,27 +13,33 @@ AWS_S3_BUCKET_NAME = 'rng-api'
 s3 = boto3.resource('s3')
 
 
-def _search(step, needles):
-    start = time()
-    encoded_needle = Gen7RNG.encode_needle(needles)
-    object_key_name = '{}/{:04X}.bin'.format(step, encoded_needle & 0xFFFF)
-
-    obj = s3.Object(AWS_S3_BUCKET_NAME, object_key_name)
-    res = obj.get()
-    db = res['Body'].read()
-    print(time() - start)
-
+def _search(step, original_needles, fuzzy=False):
     gen7_rng = Gen7RNG(step)
-    seeds = gen7_rng.search_seed(needles, db)
-    print(time() - start)
     results = []
-    for seed in seeds:
-        result = {}
-        result['seed'] = '{:08x}'.format(seed)
-        result['encoded_needle'] = '{:08x}'.format(encoded_needle)
-        result['step'] = step
 
-        results.append(result)
+    for add in range(17):
+        if not fuzzy and add != 0:
+            continue
+
+        needles = [(x + add) % 17 for x in original_needles]
+        encoded_needle = Gen7RNG.encode_needle(needles)
+        object_key_name = '{}/{:04X}.bin'.format(step, encoded_needle & 0xFFFF)
+
+        obj = s3.Object(AWS_S3_BUCKET_NAME, object_key_name)
+        res = obj.get()
+        db = res['Body'].read()
+
+        seeds = gen7_rng.search_seed(needles, db)
+
+        for seed in seeds:
+            result = {}
+            result['seed'] = '{:08x}'.format(seed)
+            result['encoded_needle'] = '{:08x}'.format(encoded_needle)
+            result['step'] = step
+            if fuzzy:
+                result['add'] = (-add + 17) % 17
+
+            results.append(result)
 
     return {'results': results}
 
@@ -42,8 +47,9 @@ def _search(step, needles):
 def main(event, context):
     needles = list(map(int, event['needle'].split(',')))
     step = int(event['step'])
+    fuzzy = event.get('fuzzy', False)
 
-    return _search(step, needles)
+    return _search(step, needles, fuzzy)
 
 
 if __name__ == '__main__':
